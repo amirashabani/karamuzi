70ec4d3d2a75a3ae025163ed77f0635754ed44f9
Amir A. Shabani <amirshabani011@gmail.com>
2019-09-24 11:17:14 +0330

    added 20 simple and distinct colors (from: https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const url = 'url';
 
-let colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
+let colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'];
 let chLine = document.getElementById("chLine");
 
 fetch(url)
