7e8567e3c47454c318a5778a386ab6e0442e2745
Amir A. Shabani <amirshabani011@gmail.com>
2019-09-24 10:26:28 +0330

    fetched data from API; chart works

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 const url = 'url';
 
+let colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
+let chLine = document.getElementById("chLine");
+
 fetch(url)
     .then((resp) => resp.json())
     .then(
         function(data) {
             console.log(data)
+            let chartData = {
+                labels: data['dates'],
+                datasets: [
+                    {
+                        data: data['topics'][0]['points'],
+                        backgroundColor: 'transparent',
+                        borderColor: colors[0],
+                        borderWidth: 1,
+                        pointBackgroundColor: colors[0]
+                    },
+                    {
+                        data: data['topics'][1]['points'],
+                        backgroundColor: 'transparent',
+                        borderColor: colors[1],
+                        borderWidth: 1,
+                        pointBackgroundColor: colors[1]
+                    }
+                ]
+            };
+              
+            if (chLine) {
+                new Chart(chLine,{
+                        type: 'line',
+                        data: chartData,
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                    beginAtZero: false
+                                    }
+                                }]
+                            },
+                            legend: {
+                                display: false
+                            }
+                        }
+                    }
+                );
+            }
         }
     )
     .catch(
         function(error) {
-            console.error(error);
+            console.error(error)
         }
-    );  
+    )
