d888db7e80cf0edcc3aad6728f1c912f3fc219f9
Amir A. Shabani <amirshabani011@gmail.com>
2019-09-28 18:04:17 +0330

    added onclick method to find out which chart was clicked on

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const url = 'url';
 let colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'];
 let chLine = document.getElementById("chLine");
 
+let chartData = {
+    datasets: []
+}
+
 fetch(url)
     .then((resp) => resp.json())
     .then(
         function(data) {
-            console.log(data)
-            let chartData = {
-                labels: data['dates'],
-                datasets: []
-            }
+            chartData['labels'] = data['dates']
 
             data['topics'].forEach(function(value, i) {
                 chartData['datasets'].push({
@@ -42,7 +42,9 @@ fetch(url)
                                 labels: {
                                     boxWidth: 5
                                 }
-                            }
+                            },
+                            events: ['click'],
+                            onClick: clicked
                         }
                     }
                 );
@@ -54,3 +56,9 @@ fetch(url)
             console.error(error)
         }
     )
+
+
+function clicked(c, i) {
+    let index = this.getElementAtEvent(c)[0]['_datasetIndex'];
+    console.log(chartData['datasets'][index]['label'])
+}
