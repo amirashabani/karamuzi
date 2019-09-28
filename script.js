97a91171d901b2a890673f91d5fdf14a8a5bf70f
Amir A. Shabani <amirshabani011@gmail.com>
2019-09-28 18:36:11 +0330

    changed variable name

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const url = 'url';
 
 let colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'];
-let chLine = document.getElementById("chLine");
+let chart = document.getElementById("chart");
 let chartData = {
     datasets: []
 }
@@ -24,29 +24,28 @@ fetch(url)
             })
         })
 
-        if (chLine) {
-            new Chart(chLine,{
-                    type: 'line',
-                    data: chartData,
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                beginAtZero: false
-                                }
-                            }]
-                        },
-                        legend: {
-                            position: 'top',
-                            labels: {
-                                boxWidth: 5
+        if (chart) {
+            new Chart(chart,{
+                type: 'line',
+                data: chartData,
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                            beginAtZero: false
                             }
-                        },
-                        events: ['click'],
-                        onClick: clicked
-                    }
+                        }]
+                    },
+                    legend: {
+                        position: 'top',
+                        labels: {
+                            boxWidth: 5
+                        }
+                    },
+                    events: ['click'],
+                    onClick: clicked
                 }
-            );
+            });
         }
     }
 )
