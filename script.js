a9e063c8075a96c32543b5e49e7d81b13f7e9e81
Amir A. Shabani <amirshabani011@gmail.com>
2019-09-28 18:58:20 +0330

    wrapped fetching and drawing into a function to reuse

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = 'url';
+const api_url = 'url';
 
 let colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'];
 let chart = document.getElementById("chart");
@@ -6,57 +6,60 @@ let chartData = {
     datasets: []
 }
 
-fetch(url)
-.then((resp) => resp.json())
-.then(
-    function(data) {
-        chartData['labels'] = data['dates']
+function draw_chart(url) {
+    fetch(url)
+    .then((resp) => resp.json())
+    .then(
+        function(data) {
+            chartData['labels'] = data['dates']
 
-        data['topics'].forEach(function(value, i) {
-            chartData['datasets'].push({
-                label: value['title'],
-                data: value['points'],
-                backgroundColor: 'transparent',
-                borderColor: colors[i],
-                borderWidth: 1,
-                pointBackgroundColor: colors[i]
+            data['topics'].forEach(function(value, i) {
+                chartData['datasets'].push({
+                    label: value['title'],
+                    data: value['points'],
+                    backgroundColor: 'transparent',
+                    borderColor: colors[i],
+                    borderWidth: 1,
+                    pointBackgroundColor: colors[i]
 
+                })
             })
-        })
 
-        if (chart) {
-            new Chart(chart,{
-                type: 'line',
-                data: chartData,
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                            beginAtZero: false
+            if (chart) {
+                new Chart(chart,{
+                    type: 'line',
+                    data: chartData,
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                beginAtZero: false
+                                }
+                            }]
+                        },
+                        legend: {
+                            position: 'top',
+                            labels: {
+                                boxWidth: 5
                             }
-                        }]
-                    },
-                    legend: {
-                        position: 'top',
-                        labels: {
-                            boxWidth: 5
-                        }
-                    },
-                    events: ['click'],
-                    onClick: clicked
-                }
-            });
+                        },
+                        events: ['click'],
+                        onClick: clicked
+                    }
+                });
+            }
         }
-    }
-)
-.catch(
-    function(error) {
-        console.error(error)
-    }
-)
-
+    )
+    .catch(
+        function(error) {
+            console.error(error)
+        }
+    )
+}
 
 function clicked(c) {
     let index = this.getElementAtEvent(c)[0]['_datasetIndex']
     console.log(chartData['datasets'][index]['label'])
 }
+
+draw_chart(api_url)
