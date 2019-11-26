bc50fdbc71c2fee1b30afa698c4e0faa9ec697d6
javad <javadkarami14@yahoo.com>
2019-11-26 19:52:36 +0330

    Add cloud layout and its draw function from https://github.com/jasondavies/d3-cloud/blob/master/examples/browserify.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let d3 = require("d3");
+let cloud = require(".");
+
 let colors = [
     "#e6194b",
     "#3cb44b",
@@ -101,6 +104,43 @@ function draw_wordcloud(url) {
                     size: 19.124779263642942
                 }
             ]
+
+            let layout = cloud()
+                .size([500, 500])
+                .words(top_ten_words)
+                .padding(5)
+                .rotate(function () {
+                    return ~~(Math.random() * 2) * 90;
+                })
+                .font("Impact")
+                .fontSize(function (d) {
+                    return d.size;
+                })
+                .on("end", draw);
+
+            layout.start();
+
+            function draw(words) {
+                d3.select("word-cloud").append("svg")
+                    .attr("width", layout.size()[0])
+                    .attr("height", layout.size()[1])
+                    .append("g")
+                    .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+                    .selectAll("text")
+                    .data(words)
+                    .enter().append("text")
+                    .style("font-size", function (d) {
+                        return d.size + "px";
+                    })
+                    .style("font-family", "Impact")
+                    .attr("text-anchor", "middle")
+                    .attr("transform", function (d) {
+                        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                    })
+                    .text(function (d) {
+                        return d.text;
+                    });
+            }
         });
 }
 
